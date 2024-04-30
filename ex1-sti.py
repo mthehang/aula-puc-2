ffb05fc7be2ddafb654cd081e2d3f821a789052d
mthehang <matheus@vts.com.br>
2024-04-30 12:01:53 -0300

    ex em sala

---
+++ ex1-sti.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+matriz = np.random.randint(2, size=(4, 4))
+
+print('Faça uma ista de 4 números com 0 e 1! ')
+lista = []
+i = 0
+while i < 4:
+    lista.append(int(input(f'Digite o {i + 1}° número: ')))
+    i += 1
+
+print(f"\n{matriz}\n")
+
+for i in range(len(matriz)):
+    if np.array_equal(matriz[i], lista):
+        print("Jogador venceu!")
+        exit()
+    else:
+        pass
+
+print("Jogador perdeu!")
+
+
+
+
